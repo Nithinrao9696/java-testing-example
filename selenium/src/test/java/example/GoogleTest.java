08fee000322992630c6cf094d90b1028e114df05
Nithinrao9696 <30919697+Nithinrao9696@users.noreply.github.com>
2020-02-20 14:50:57 -0500

    Update GoogleTest.java

--- selenium/src/test/java/example/GoogleTest.java
+++ selenium/src/test/java/example/GoogleTest.java
@@ -1,24 +1,74 @@
+Skip to content
+Search or jump to…
 
-public class GridExample {
-     
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@Nithinrao9696 
+Learn Git and GitHub without any code!
+Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.
+
+
+joeg3
+/
+selenium-grid-example
+2
+00
+ Code Issues 0 Pull requests 0 Actions Projects 0 Wiki Security Insights
+selenium-grid-example/src/test/java/org/example/seleniumgrid/SearchTest.java
+@joeg3 joeg3 Add package for page objects
+a55fb4c on Oct 25, 2016
+38 lines (31 sloc)  1.03 KB
+  
+package org.example.seleniumgrid;
+
+import static org.junit.Assert.assertEquals;
+
+import org.example.seleniumgrid.pageobjects.HomePage;
+import org.example.seleniumgrid.pageobjects.SearchResultsPage;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+  
+public class SearchTest {
+    
+    WebDriver driver;
+    HomePage homepage;
+    SearchResultsPage searchResultsPage;
+    
+    @Before
+    public void setup() {
+        driver = new FirefoxDriver();
+        homepage = new HomePage(driver, "https://www.duckduckgo.com");
+        //homepage.get();
+    }
+    
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+    
     @Test
-    public void mailTest() throws MalformedURLException{
-        DesiredCapabilities dr=null;
-        if(browserType.equals("firefox")){
-        dr=DesiredCapabilities.firefox();
-        dr.setBrowserName("firefox");
-        dr.setPlatform(Platform.WINDOWS);
-     
-        }else{
-            dr=DesiredCapabilities.internetExplorer();
-            dr.setBrowserName("iexplore");
-            dr.setPlatform(Platform.WINDOWS);
-        }
-             
-        RemoteWebDriver driver=new RemoteWebDriver(new    URL("http://ap-jenkdx-dev:4444/wd/hub"), dr);
-        driver.navigate().to("http://gmail.com");
-        driver.findElement(By.xpath("//input[@id='Email']")) .sendKeys("username");
-        driver.findElement(By.xpath("//input[@id='Passwd']")) .sendKeys("password");
-        driver.close();
-     
+    public void testSearch() throws Exception {
+        //homepage.isLoaded(); // Throws error if not loaded
+        assertEquals("DuckDuckGo", homepage.getTitle());
+        searchResultsPage = homepage.search("packers");
+        assertEquals("packers at DuckDuckGo", searchResultsPage.getTitle());
+  }
 }
+© 2020 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
